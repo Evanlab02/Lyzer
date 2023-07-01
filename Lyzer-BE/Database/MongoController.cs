e4c086f1c922c12cda66b5105957033bdc23c806
Pranav Reddy <ggpranav44@gmail.com>
2023-07-01 15:42:14 +0200

    feat: Init setup of MongoController

---
+++ Lyzer-BE/Database/MongoController.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using dotenv.net.Utilities;
+using System.Diagnostics;
+
+namespace Lyzer_BE.Database
+{
+    public class MongoController
+    {
+        public static MongoClient dbClient;
+        
+        public static void CreateMongoDBClient()
+        {
+            string connectionUri = EnvReader.GetStringValue("MONGODB_CONNECTION");
+            var settings = MongoClientSettings.FromConnectionString(connectionUri);
+
+            // Set the ServerApi field of the settings object to Stable API version 1
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+
+            // Create a new client and connect to the server
+            dbClient = new MongoClient(settings);
+
+            // Send a ping to confirm a successful connection
+            try
+            {
+                var result = dbClient.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+                Debug.WriteLine("LyzerDB: Pinged your deployment. You successfully connected to MongoDB!");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"LyzerDB: {ex}");
+            }
+        }
+    }
+}
