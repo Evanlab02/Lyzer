5461dcc5d3e568d2ea4110c90605db6e9e027b55
Pranav Reddy <ggpranav44@gmail.com>
2023-07-03 07:09:55 +0200

    chore(Hydration): using DateTime.Now for Schedule Hydration

--- Lyzer-BE/API/Services/Concrete/HydrationService.cs
+++ Lyzer-BE/API/Services/Concrete/HydrationService.cs
@@ -15,28 +15,34 @@ namespace Lyzer_BE.API.Services.Concrete
             var request = new RestRequest($"current.json");
             var response = await client.GetAsync<ScheduleDTO>(request);
 
-            MongoController<RaceWeekendDTO> mongoController = new("Schedules", "2023");
+            MongoController<RaceWeekendDTO> mongoController = new("Schedules", DateTime.Now.Year.ToString());
             var filterValues = Builders<RaceWeekendDTO>.Filter.Empty;
             mongoController.DeleteManyFromCollection(filterValues);
 
             mongoController.InsertManyIntoCollection(response.ScheduleData.ScheduleTable.RaceWeekends);
+
             return response;
         }
 
         public async Task<ScheduleDTO> HydrateFollowingYearSchedule()
         {
+            var nextYear = DateTime.Now.AddYears(1).Year.ToString();
+
             var options = new RestClientOptions("http://ergast.com/api/f1/");
             var client = new RestClient(options);
-            var request = new RestRequest($"2024.json");
+            var request = new RestRequest($"{nextYear}.json");
             var response = await client.GetAsync<ScheduleDTO>(request);
 
-            MongoController<RaceWeekendDTO> mongoController = new("Schedules", "2024");
-            var filterValues = Builders<RaceWeekendDTO>.Filter.Empty;
-            mongoController.DeleteManyFromCollection(filterValues);
-
-            if (response.ScheduleData.ScheduleTable.RaceWeekends.Count != 0)
+            if (response.ScheduleData.ScheduleTable.RaceWeekends.Count > 0)
             {
-                mongoController.InsertManyIntoCollection(response.ScheduleData.ScheduleTable.RaceWeekends);
+                MongoController<RaceWeekendDTO> mongoController = new("Schedules", nextYear);
+                var filterValues = Builders<RaceWeekendDTO>.Filter.Empty;
+                mongoController.DeleteManyFromCollection(filterValues);
+
+                if (response.ScheduleData.ScheduleTable.RaceWeekends.Count != 0)
+                {
+                    mongoController.InsertManyIntoCollection(response.ScheduleData.ScheduleTable.RaceWeekends);
+                }
             }
 
             return response;
