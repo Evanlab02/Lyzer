54ac58fb1ed5a1dd909cbe0b306eccf3797cd406
Brendon Engelbrecht <bengelbrecht2002@gmail.com>
2024-12-28 15:20:30 +0200

    Reverted previous Push. Created endpoint for races

---
+++ Backend/Lyzer/Common/DTO/LocationDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Lyzer.Common.DTO
+{
+    public class LocationDTO
+    {
+        public required string Lat { get; set; }
+        public required string Long { get; set; }
+        public required string Locality { get; set; }
+        public required string Country { get; set; }
+    }
+}
---
+++ Backend/Lyzer/Common/DTO/ResultsDTO.cs
@@ -0,0 +1,46 @@
+﻿namespace Lyzer.Common.DTO
+{
+    public class ResultsDTO
+    {
+        public required string Season { get; set; }
+        public required string Round { get; set; }
+        public required string Url { get; set; }
+
+        public required string RaceName { get; set; }
+        public required CircuitDTO Circuit { get; set; }
+        public required string Date { get; set; }
+        public required string Time { get; set; }
+        public required List<ResultDTO> Results { get; set; }
+    }
+    public class ResultDTO
+    {
+        public required string Number { get; set; }
+        public required string Position { get; set; }
+        public required string PositionText { get; set; }
+        public required string Points { get; set; }
+        public required DriverDTO Driver { get; set; }
+        public required ConstructorDTO Constructor { get; set; }
+        public required string Grid { get; set; }
+        public required string Laps { get; set; }
+        public required string Status { get; set; }
+        public required ResultTimeDTO Time { get; set; }
+        public required ResultFastestLapDTO FastestLap { get; set; }
+    }
+    public class ResultTimeDTO
+    {
+        public string? Millis { get; set; }
+        public required string Time { get; set; }
+    }
+    public class ResultFastestLapDTO
+    {
+        public required string rank { get; set; }
+        public required string lap { get; set; }
+        public required ResultTimeDTO Time { get; set; }
+        public required ResultAverageSpeedDTO AverageSpeed { get; set; }
+    }
+    public class ResultAverageSpeedDTO
+    {
+        public required string Units { get; set; }
+        public required string Speed { get; set; }
+    }
+}
