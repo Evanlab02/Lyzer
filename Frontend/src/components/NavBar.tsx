60454c5c400a83d00e6a99b50aa0b3d99acac952
Pranav-Zodes-44 <ggpranav44@gmail.com>
2025-01-13 22:59:39 +0200

    chore(Navbar): Using Lucide icons instead of material pngs

--- Frontend/src/components/NavBar.tsx
+++ Frontend/src/components/NavBar.tsx
@@ -1,7 +1,9 @@
+import { Menu, Moon, Sun } from "lucide-react";
 import { useTheme } from "../hooks/useTheme";
+import "../styles/navbar.scss";
 
 type NavBarProps = {
-	onMenuClick: (event: React.MouseEvent<HTMLDivElement>) => void;
+	onMenuClick: (event: React.MouseEvent<SVGElement>) => void;
 }
 
 export default function NavBar({ onMenuClick}: NavBarProps) {
@@ -9,19 +11,34 @@ export default function NavBar({ onMenuClick}: NavBarProps) {
 
 	return (
 		<div className="navbar">
-			<div onClick={onMenuClick} id="menu-button" className="icon menu">
-				<img src={isDarkMode ? "/menu_white.png" : "/menu_black.png"} className="icon"></img>
-			</div>
+			<Menu 
+				color={isDarkMode ? "white" : "black"} 
+				size={28} onClick={onMenuClick} 
+				className="icon menu"
+				id="navbar-menu"
+			/>
 			<div>
 				​🇱​​🇾​​🇿​​🇪​​🇷​
 			</div>
-			<div>
-				<img className="icon theme-toggle"
-					src={isDarkMode ? "/dark_mode.png" : "/light_mode.png"}
-					id="theme-toggle"
-					onClick={() => {toggleTheme()}}
-				></img>
-			</div>
+			{
+				isDarkMode ? (
+					<Moon
+						id="theme-toggle-dark"
+						className="icon theme-toggle"
+						color="white"
+						size={28}
+						onClick={() => toggleTheme()}
+					/>
+				) : (
+					<Sun
+						id="theme-toggle-light"
+						className="icon theme-toggle"
+						color="black"
+						size={28}
+						onClick={() => toggleTheme()}
+					/>
+				)
+			}
 		</div>
 	);
 }
