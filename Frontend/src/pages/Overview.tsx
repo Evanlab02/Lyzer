4efca144d68bdff814eca3eeec43ceb2d2688c19
Evanlab02 <evanlabuschagne70@gmail.com>
2025-02-02 22:41:06 +0200

    chore: Format

--- Frontend/src/pages/Overview.tsx
+++ Frontend/src/pages/Overview.tsx
@@ -3,7 +3,7 @@ import { GridContainer, GridItem } from "../components/Grid";
 export default function Overview() {
 	return (
 		<div data-testid="lyzer-overview-page">
-            <GridContainer>
+			<GridContainer>
 				<GridItem xs={12} sm={12} md={6} lg={6} xl={4} xxl={4}>
 					<div>IDK</div>
 				</GridItem>
