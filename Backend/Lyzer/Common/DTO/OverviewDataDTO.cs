632a7b3df73aa831eca7b406da0180f7ca309e31
Pranav Reddy <ggpranav44@gmail.com>
2025-02-05 20:56:47 +0200

    chore(Overview): Creating OverviewDataDTO

---
+++ Backend/Lyzer/Common/DTO/OverviewDataDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Lyzer.Common.DTO
+{
+    public class OverviewDataDTO
+    {
+        public required RaceWeekendProgressDTO RaceWeekendProgress { get; set; }
+    }
+
+    public class RaceWeekendProgressDTO
+    {
+        public required string Name { get; set; }
+        public required bool Ongoing { get; set; }
+        public required int WeekendProgress { get; set; }
+    }
+}
