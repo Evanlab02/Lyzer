4579adaf57d61d97f098ac8caa8c5071b1a28be8
Evanlab02 <evanlabuschagne70@gmail.com>
2025-02-05 20:37:29 +0200

    feat(Frontend): Race weekend progress card

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
