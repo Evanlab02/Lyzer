52622f7bbef183525805a164d68b0df8e8c5d2b6
Pranav Reddy <ggpranav44@gmail.com>
2025-02-11 01:33:19 +0200

    feature: Extension for us to get Enum Value if we have the description value

---
+++ Backend/Lyzer/Common/Extensions/Enums.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace Lyzer.Common.Extensions
+{
+    public static class Enums
+    {
+        public static int? GetValueFromEnumDescription<TEnum>(string description) where TEnum : Enum
+        {
+            var type = typeof(TEnum);
+
+            foreach (var field in type.GetFields())
+            {
+                var attribute = field.GetCustomAttribute<DescriptionAttribute>();
+                if (attribute != null && attribute.Description == description)
+                {
+                    return (int)field.GetValue(null)!;
+                }
+            }
+
+            return null;
+        }
+    }
+}
