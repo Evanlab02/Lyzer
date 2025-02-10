95f26b16e4cee65484b59d8ada072636fab26246
Pranav Reddy <ggpranav44@gmail.com>
2025-02-11 01:32:56 +0200

    chore: Max Session Time in minutes
    For us to reference whenever we need to get the max end date of a session

---
+++ Backend/Lyzer/Common/Constants/RaceConstants.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace Lyzer.Common.Constants
+{
+    public enum MaxSessionTimeConstants
+    {
+        [Description("Practice")]
+        Practice = 60,
+        [Description("Qualifying")]
+        Qualifying = 60,
+        [Description("Sprint Qualifying")]
+        SprintQualifying = 44,
+        [Description("Sprint")]
+        Sprint = 45,
+        [Description("Race")]
+        Race = 180,
+    }
+}
