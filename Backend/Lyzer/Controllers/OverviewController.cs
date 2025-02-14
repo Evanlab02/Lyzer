76e594ddd4cba4229b05ee549beb14a238f8d9db
Pranav Reddy <ggpranav44@gmail.com>
2025-02-14 13:31:10 +0200

    feature: GetOverviewData

--- Backend/Lyzer/Controllers/OverviewController.cs
+++ Backend/Lyzer/Controllers/OverviewController.cs
@@ -10,16 +10,24 @@ namespace Lyzer.Controllers
     public class OverviewController : ControllerBase
     {
         private readonly RacesService _raceService;
+        private readonly OverviewService _overviewService;
 
-        public OverviewController(RacesService raceService)
+        public OverviewController(RacesService raceService, OverviewService overviewService)
         {
             _raceService = raceService;
+            _overviewService = overviewService;
         }
 
-        [HttpGet("", Name = "GetUpcomingRaceWeekend")]
+        [HttpGet("raceweekend/upcoming", Name = "GetUpcomingRaceWeekend")]
         public async Task<UpcomingRaceWeekendDTO> GetUpcomingRaceWeekend()
         {
             return await _raceService.GetUpcomingRaceWeekend("current");
+        }
+
+        [HttpGet("", Name = "GetOverviewData")]
+        public async Task<OverviewDataDTO> GetOverviewData()
+        {
+            return await _overviewService.GetOverviewData();
         }
     }
 }
