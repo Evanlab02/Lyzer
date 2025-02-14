707423e04363d494029abde9381b0b947d62fe40
Pranav Reddy <ggpranav44@gmail.com>
2025-02-14 12:09:49 +0200

    chore: Comments explaining the logic.

--- Backend/Lyzer/Clients/JolpicaClient.cs
+++ Backend/Lyzer/Clients/JolpicaClient.cs
@@ -122,6 +122,12 @@ namespace Lyzer.Clients
             {
                 List<SessionDTO?> allSessions = new List<SessionDTO?> { race.FirstPractice, race.SecondPractice, race.ThirdPractice, race.Qualifying, race.Sprint, race.SprintQualifying };
 
+                /*
+                 * 1. Creates an array of tuples, where item 1 is the SessionDTO? and item 2 is a string of the human readable session name
+                 * 2. Filters out the null sessions, as most races don't have all the sessions
+                 * 3. Converts it to a KeyValuePair/Dictionary, where we can get the session name by reference of the object
+                 */
+
                 var sessionNames = new Dictionary<SessionDTO, string>(
                     new[]
                     {
@@ -135,6 +141,11 @@ namespace Lyzer.Clients
                     .Where(x => x.Item1 != null)
                     .Select(x => new KeyValuePair<SessionDTO, string>(x.Item1!, x.Item2))
                 );
+
+                /*
+                 * 1. Filters out all the null sessions
+                 * 2. Sets the session name, via the dictionary we created above.
+                 */
 
                 race.Sessions = allSessions
                     .Where(x => x != null)
