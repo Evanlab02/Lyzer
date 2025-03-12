c492a987999ec5240911e61dc8806406b2e771ab
Pranav Reddy <ggpranav44@gmail.com>
2025-03-12 20:18:00 +0200

    chore: Adding driverStandings to overview endpoint

--- Backend/Lyzer/Services/OverviewService.cs
+++ Backend/Lyzer/Services/OverviewService.cs
@@ -5,21 +5,25 @@ namespace Lyzer.Services
     public class OverviewService
     {
         private readonly RacesService _racesService;
+        private readonly DriverService _driverService;
 
-        public OverviewService(RacesService racesService)
+        public OverviewService(RacesService racesService, DriverService driverService)
         {
             _racesService = racesService;
+            _driverService = driverService;
         }
 
         public async Task<OverviewDataDTO> GetOverviewData()
         {
             UpcomingRaceWeekendDTO upcomingRaceWeekend = await _racesService.GetUpcomingRaceWeekend();
             RaceWeekendProgressDTO raceWeekendProgress = await _racesService.GetRaceWeekendProgress();
+            List<OverviewDriverDTO> drivers = await _driverService.GetCurrentDriverStandings();
 
             return new OverviewDataDTO
             {
                 RaceWeekendProgress = raceWeekendProgress,
                 UpcomingRaceWeekend = upcomingRaceWeekend,
+                Drivers = drivers
             };
         }
     }
